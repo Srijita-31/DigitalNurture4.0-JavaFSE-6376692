f1fa64f30c1418091bd7b83cdcf3851970b5d7dd
Srijita <srijita.ghorai2003@gmail.com>
2025-06-19 19:28:12 +0530

    Update ExcelFactory.java

--- Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/ExcelFactory.java
+++ Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/ExcelFactory.java
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 public class ExcelFactory extends DocumentFactory {
     @Override
     public Document createDocument() {
         return new ExcelDocument();
     }
 }
-=======
-public class ExcelFactory extends DocumentFactory {
-    @Override
-    public Document createDocument() {
-        return new ExcelDocument();
-    }
-}
->>>>>>> 27696d9c0a434ba9acda1599f10089aae0616e67
+
