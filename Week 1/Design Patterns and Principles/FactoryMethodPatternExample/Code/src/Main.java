465302125fa5ed7d29aa4b02fcbcbec6daee1c18
Srijita <srijita.ghorai2003@gmail.com>
2025-06-19 19:29:15 +0530

    Update Main.java

--- Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/Main.java
+++ Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Main {
@@ -22,26 +21,7 @@ public class Main {
         }
     }
 }
-=======
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter document type (word/pdf/excel): ");
-            String type = sc.nextLine();
-            
-            DocumentFactory factory;
-            
-            if (type.equalsIgnoreCase("word")) {
-                factory = new WordFactory();
-            } else if (type.equalsIgnoreCase("pdf")) {
-                factory = new PdfFactory();
-            } else {
-                factory = new ExcelFactory();
-            }
-            
-            Document doc = factory.createDocument();
             doc.open();
         }
     }
