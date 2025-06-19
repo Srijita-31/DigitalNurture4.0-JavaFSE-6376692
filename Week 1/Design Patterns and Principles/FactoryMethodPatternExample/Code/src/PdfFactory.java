fe67104d9c55726c8ff13ef8726384fe800e6d11
Srijita <srijita.ghorai2003@gmail.com>
2025-06-19 19:29:56 +0530

    Update PdfFactory.java

--- Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/PdfFactory.java
+++ Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/PdfFactory.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 public class PdfFactory extends DocumentFactory {
     @Override
     public Document createDocument() {
         return new PdfDocument();
     }
 }
-=======
-public class PdfFactory extends DocumentFactory {
-    @Override
-    public Document createDocument() {
-        return new PdfDocument();
-    }
-}
->>>>>>> 27696d9c0a434ba9acda1599f10089aae0616e67
