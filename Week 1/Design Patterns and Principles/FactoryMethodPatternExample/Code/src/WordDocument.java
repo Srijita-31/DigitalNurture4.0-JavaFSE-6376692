bb42c949e60eab8c902c0f1e59c835e4d7f3506d
Srijita <srijita.ghorai2003@gmail.com>
2025-06-19 19:30:13 +0530

    Update WordDocument.java

--- Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/WordDocument.java
+++ Week 1/Design Patterns and Principles/FactoryMethodPatternExample/Code/src/WordDocument.java
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 public class WordDocument implements Document {
     @Override
     public void open() {
         System.out.println("Opening Word document.");
     }
-=======
-public class WordDocument implements Document {
-    @Override
-    public void open() {
-        System.out.println("Opening Word document.");
-    }
->>>>>>> 27696d9c0a434ba9acda1599f10089aae0616e67
-}
